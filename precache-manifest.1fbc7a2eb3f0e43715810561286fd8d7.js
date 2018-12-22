1030c9654d32b4306b7948e162af94934dc1e277
teffcode <tefy.1@live.com>
2018-12-22 09:38:25 -0500

    Updates

---
+++ precache-manifest.1fbc7a2eb3f0e43715810561286fd8d7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7b1688d4473f023356ee3558376140",
+    "url": "/teffcode/static/media/meet10.8b7b1688.jpeg"
+  },
+  {
+    "revision": "2ae46883289795318400",
+    "url": "/teffcode/static/css/main.eff8412c.chunk.css"
+  },
+  {
+    "revision": "45f49d8e18aa5c4feffb",
+    "url": "/teffcode/static/js/1.45f49d8e.chunk.js"
+  },
+  {
+    "revision": "f229921ea01699dde40f",
+    "url": "/teffcode/static/js/runtime~main.f229921e.js"
+  },
+  {
+    "revision": "42bba541983b6f921da81683c5205fda",
+    "url": "/teffcode/static/media/meet1.42bba541.jpeg"
+  },
+  {
+    "revision": "813829e4e6fe480399096dfdea6aae12",
+    "url": "/teffcode/static/media/meet2.813829e4.jpeg"
+  },
+  {
+    "revision": "ba9b595f2d7a57a1f4cd85e5a4ce5b75",
+    "url": "/teffcode/static/media/meet3.ba9b595f.jpeg"
+  },
+  {
+    "revision": "2ac5b9599a36b11fdeedec29a46524b2",
+    "url": "/teffcode/static/media/meet4.2ac5b959.jpeg"
+  },
+  {
+    "revision": "e71805f4e3d932401496cc87e333469b",
+    "url": "/teffcode/static/media/meet5.e71805f4.jpeg"
+  },
+  {
+    "revision": "4055b9a4b06fc8e9e62b1df381e47252",
+    "url": "/teffcode/static/media/meet6.4055b9a4.jpeg"
+  },
+  {
+    "revision": "0829c7fa4d5e742ed9dd4de44ca8f9f4",
+    "url": "/teffcode/static/media/meet7.0829c7fa.jpeg"
+  },
+  {
+    "revision": "6918383de93ec2e0dd7545d140bef8fa",
+    "url": "/teffcode/static/media/meet8.6918383d.jpeg"
+  },
+  {
+    "revision": "4df5ad922af88d865bc19f34871386a2",
+    "url": "/teffcode/static/media/meet9.4df5ad92.jpeg"
+  },
+  {
+    "revision": "2ae46883289795318400",
+    "url": "/teffcode/static/js/main.2ae46883.chunk.js"
+  },
+  {
+    "revision": "cf31caf88f403bd1917e2f0471fa874e",
+    "url": "/teffcode/static/media/sass.cf31caf8.png"
+  },
+  {
+    "revision": "1317df5b63c7b76a678402d980e0b603",
+    "url": "/teffcode/static/media/splash-ionic.1317df5b.png"
+  },
+  {
+    "revision": "bb73a2211e3365117b7320cd0d2ac250",
+    "url": "/teffcode/static/media/react.bb73a221.png"
+  },
+  {
+    "revision": "7eda64a06a3f93753300f031e88f4cc4",
+    "url": "/teffcode/static/media/layout.7eda64a0.png"
+  },
+  {
+    "revision": "806384a076a18469fc15f3e1ee002bed",
+    "url": "/teffcode/static/media/emoji.806384a0.png"
+  },
+  {
+    "revision": "2204a77ee55600082a2d28c2e61c2f90",
+    "url": "/teffcode/static/media/jsconf.2204a77e.png"
+  },
+  {
+    "revision": "7ed3ef9dd1db582a26cd3661ba3f5c6d",
+    "url": "/teffcode/static/media/howtobegin.7ed3ef9d.png"
+  },
+  {
+    "revision": "7ad046afa587e840903509dcf9061afb",
+    "url": "/teffcode/static/media/viewencapsulation.7ad046af.png"
+  },
+  {
+    "revision": "f7b7dbe3da6263baad4000f8d5d4f712",
+    "url": "/teffcode/static/media/cssgrid.f7b7dbe3.png"
+  },
+  {
+    "revision": "d0634e22b2f044f56e6560d724665f49",
+    "url": "/teffcode/static/media/react.d0634e22.png"
+  },
+  {
+    "revision": "d172d119bb95f1b899fcea86b65633da",
+    "url": "/teffcode/static/media/cssanimations.d172d119.png"
+  },
+  {
+    "revision": "b19be569e1e5a0ada0c5b3a2d215aabd",
+    "url": "/teffcode/index.html"
+  }
+];
